21321d10af172fcb64004a597b87d68d5ae88adb
中島浩二 <knaka0209@gmail.com>
2021-01-18 18:26:23 +0900

    up,0118a6

--- components/Footer.js
+++ components/Footer.js
@@ -9,6 +9,17 @@ export default function(){
       <a><p className="p_foot_str">About</p>
       </a>
     </Link>
+    <style>{`
+    .p_foot_str {
+      color: #FFF;
+    }
+    .footer_box {
+      margin-top: 20px;
+      background-color: #757575;
+      color: #fff;
+      padding: 140px 40px;
+    }
+    `}</style> 
   </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import cookies from 'next-cookies'
 
 import Layout from '../components/layout'
+import LoginBox from '../components/LoginBox'
 //import LibCommon from '../libs/LibCommon'
 //
-export default function Home() {
-  return (
-  <Layout>
-    <div className="container">
+function Home(data) {
+  console.log( "user_id=", data.user_id )
+    return (
+    <Layout>
+      <LoginBox user_id={data.user_id} />
+      <div className="container">
+        <h1>Home</h1>
+        <p>This is home page.</p>
+      </div>
       <hr />
-      <h1>Home</h1>
-      <p>This is home page.</p>
-    </div>
-  </Layout>
-  )
-}
+    </Layout>
+    )
+  }
+  //
+  Home.getInitialProps = async (ctx) => {
+    console.log("uid=", cookies(ctx).user_id)
+    return { user_id: cookies(ctx).user_id }
+  }
+  
+  export default Home
